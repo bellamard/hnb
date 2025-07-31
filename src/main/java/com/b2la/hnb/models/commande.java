69bb63f65389528127d0840e8cec015f2fe54109
B2LA <belamard@gmail.com>
2025-07-31 16:01:19 +0100

    feat(fr):ajoute dao de la commande

--- src/main/java/com/b2la/hnb/models/commande.java
+++ src/main/java/com/b2la/hnb/models/commande.java
@@ -21,4 +21,63 @@ public class commande {
     @JoinColumn(name = "idFacture", referencedColumnName = "id")
     facturation facturation;
 
+    public commande() {
+    }
+
+    public commande(Long id, String nom, double prix_unitaire, int nombre, double prix_total, facturation facturation) {
+        this.id = id;
+        this.nom = nom;
+        this.prix_unitaire = prix_unitaire;
+        this.nombre = nombre;
+        this.prix_total = prix_total;
+        this.facturation = facturation;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public double getPrix_unitaire() {
+        return prix_unitaire;
+    }
+
+    public void setPrix_unitaire(double prix_unitaire) {
+        this.prix_unitaire = prix_unitaire;
+    }
+
+    public int getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(int nombre) {
+        this.nombre = nombre;
+    }
+
+    public double getPrix_total() {
+        return prix_total;
+    }
+
+    public void setPrix_total(double prix_total) {
+        this.prix_total = prix_total;
+    }
+
+    public facturation getFacturation() {
+        return facturation;
+    }
+
+    public void setFacturation(facturation facturation) {
+        this.facturation = facturation;
+    }
 }
