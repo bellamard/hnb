141adf21e28db2e85d0763fb190e6e1cf51026f8
B2LA <belamard@gmail.com>
2025-07-31 16:36:16 +0100

    feat(fr): ajoute dao depense

--- src/main/java/com/b2la/hnb/models/depense.java
+++ src/main/java/com/b2la/hnb/models/depense.java
@@ -24,4 +24,82 @@ public class depense {
     @CreationTimestamp
     @Temporal(TemporalType.TIMESTAMP)
     Date date;
+
+    public depense() {
+    }
+
+    public depense(Long id, String intitule, String motif, String auteur, boolean is_valide, boolean is_canceled, utilisateurs utilisateur, Date date) {
+        this.id = id;
+        this.intitule = intitule;
+        this.motif = motif;
+        this.auteur = auteur;
+        this.is_valide = is_valide;
+        this.is_canceled = is_canceled;
+        this.utilisateur = utilisateur;
+        this.date = date;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getIntitule() {
+        return intitule;
+    }
+
+    public void setIntitule(String intitule) {
+        this.intitule = intitule;
+    }
+
+    public String getMotif() {
+        return motif;
+    }
+
+    public void setMotif(String motif) {
+        this.motif = motif;
+    }
+
+    public String getAuteur() {
+        return auteur;
+    }
+
+    public void setAuteur(String auteur) {
+        this.auteur = auteur;
+    }
+
+    public boolean isIs_valide() {
+        return is_valide;
+    }
+
+    public void setIs_valide(boolean is_valide) {
+        this.is_valide = is_valide;
+    }
+
+    public boolean isIs_canceled() {
+        return is_canceled;
+    }
+
+    public void setIs_canceled(boolean is_canceled) {
+        this.is_canceled = is_canceled;
+    }
+
+    public utilisateurs getUtilisateur() {
+        return utilisateur;
+    }
+
+    public void setUtilisateur(utilisateurs utilisateur) {
+        this.utilisateur = utilisateur;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
 }
