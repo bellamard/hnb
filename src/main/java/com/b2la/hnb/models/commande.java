cbfcb56c7aa96f326e606bf779baa34e4ad76b71
B2LA <belamard@gmail.com>
2025-07-31 15:56:29 +0100

    feat(fr): ajouter model table commande

--- src/main/java/com/b2la/hnb/models/commande.java
+++ src/main/java/com/b2la/hnb/models/commande.java
@@ -5,4 +5,20 @@ import jakarta.persistence.*;
 @Entity
 @Table(name = "Commandes")
 public class commande {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
+    @Column(nullable = false)
+    String nom;
+    @Column(nullable = false)
+    double prix_unitaire;
+    @Column(nullable = false)
+    int nombre;
+    @Column(nullable = false)
+    double prix_total;
+    @ManyToOne
+    @JoinColumn(name = "idFacture", referencedColumnName = "id")
+    facturation facturation;
+
 }
--- src/main/java/com/b2la/hnb/models/facturation.java
+++ src/main/java/com/b2la/hnb/models/facturation.java
@@ -39,4 +39,72 @@ public class facturation {
     @Temporal(TemporalType.TIMESTAMP)
     Date dateFacturation;
 
+    public facturation() {
+    }
+
+    public facturation(Long id, Long code_reference, double ttc, etat etat, utilisateurs utilisateur, List<produits> produit, Date dateFacturation) {
+        this.id = id;
+        this.code_reference = code_reference;
+        this.ttc = ttc;
+        this.etat = etat;
+        this.utilisateur = utilisateur;
+        this.produit = produit;
+        this.dateFacturation = dateFacturation;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getCode_reference() {
+        return code_reference;
+    }
+
+    public void setCode_reference(Long code_reference) {
+        this.code_reference = code_reference;
+    }
+
+    public double getTtc() {
+        return ttc;
+    }
+
+    public void setTtc(double ttc) {
+        this.ttc = ttc;
+    }
+
+    public etat getEtat() {
+        return etat;
+    }
+
+    public void setEtat(etat etat) {
+        this.etat = etat;
+    }
+
+    public utilisateurs getUtilisateur() {
+        return utilisateur;
+    }
+
+    public void setUtilisateur(utilisateurs utilisateur) {
+        this.utilisateur = utilisateur;
+    }
+
+    public List<produits> getProduit() {
+        return produit;
+    }
+
+    public void setProduit(List<produits> produit) {
+        this.produit = produit;
+    }
+
+    public Date getDateFacturation() {
+        return dateFacturation;
+    }
+
+    public void setDateFacturation(Date dateFacturation) {
+        this.dateFacturation = dateFacturation;
+    }
 }
