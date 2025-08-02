8d872dcdcf1904a65f098b28f6c77d57ff7e65b0
B2LA <belamard@gmail.com>
2025-08-02 09:55:15 +0100

    ajout service promotion

---
+++ src/main/java/com/b2la/hnb/services/promotionService.java
@@ -0,0 +1,28 @@
+package com.b2la.hnb.services;
+
+import com.b2la.hnb.models.promotion;
+
+import java.util.List;
+
+public class promotionService {
+
+    public void save (promotion promotion){
+
+    }
+
+    public void update (promotion promotion){
+
+    }
+
+    public promotion findById(Long id){
+
+    }
+
+    public List<promotion> findAll(){
+
+    }
+
+    public void delete(Long id){
+
+    }
+}
