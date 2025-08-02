6b9335584623160c661de2ff9c6b06d5a2f95f82
B2LA <belamard@gmail.com>
2025-08-02 09:07:54 +0100

    feat(fr): ajout fonction update sur produit service

--- src/main/java/com/b2la/hnb/services/produitService.java
+++ src/main/java/com/b2la/hnb/services/produitService.java
@@ -18,11 +18,23 @@ public class produitService {
         } catch (RuntimeException e) {
             throw new RuntimeException(e);
         }finally {
-
+            em.close();
         }
     }
     public void update(produits produit){
-
+        EntityManager em= JPAUtil.getEntityManager();
+        EntityTransaction transaction= null;
+        try{
+            transaction= em.getTransaction();
+            transaction.begin();
+            em.merge(produit);
+            transaction.commit();
+        } catch (RuntimeException e) {
+            if(transaction!=null)transaction.rollback();
+            throw new RuntimeException(e);
+        }finally {
+            em.close();
+        }
     }
     public produits produit(Long id){
 
