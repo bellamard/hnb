781de45837e235e7972ab5cc0e2f08f9a32749de
B2LA <belamard@gmail.com>
2025-08-02 09:58:44 +0100

    ajout fonction save sur service promotion

--- src/main/java/com/b2la/hnb/services/promotionService.java
+++ src/main/java/com/b2la/hnb/services/promotionService.java
@@ -1,13 +1,28 @@
 package com.b2la.hnb.services;
 
 import com.b2la.hnb.models.promotion;
+import com.b2la.hnb.util.JPAUtil;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityTransaction;
 
 import java.util.List;
 
 public class promotionService {
 
-    public void save (promotion promotion){
-
+    public void save (promotion promo){
+        EntityManager em= JPAUtil.getEntityManager();
+        EntityTransaction transaction= null;
+        try{
+            transaction= em.getTransaction();
+            transaction.begin();
+            em.persist(promo);
+            transaction.commit();
+        } catch (RuntimeException e) {
+            if(transaction!=null)transaction.rollback();
+            throw new RuntimeException(e);
+        }finally {
+            em.close();
+        }
     }
 
     public void update (promotion promotion){
