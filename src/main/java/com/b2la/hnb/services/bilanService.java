70e8b920f2fdd8fcd68f9ddd675de8d13adbfcac
B2LA <belamard@gmail.com>
2025-08-02 12:17:37 +0100

    ajout fonction findById sur  service bilan

--- src/main/java/com/b2la/hnb/services/bilanService.java
+++ src/main/java/com/b2la/hnb/services/bilanService.java
@@ -40,8 +40,18 @@ public class bilanService {
             em.close();
         }
     }
-    public bilan findById(Long id){
 
+    public bilan findById(Long id){
+        EntityManager em= JPAUtil.getEntityManager();
+        bilan bil= null;
+        try{
+            bil=em.find(bilan.class, id);
+        } catch (RuntimeException e) {
+            throw new RuntimeException(e);
+        }finally {
+            em.close();
+        }
+        return bil;
     }
 
     public List<bilan> findAll(){
