78d5cfc41223082666720a78bced431e849cdc12
B2LA <belamard@gmail.com>
2025-08-02 10:33:32 +0100

    ajout promotion facturation

---
+++ src/main/java/com/b2la/hnb/services/facturationService.java
@@ -0,0 +1,28 @@
+package com.b2la.hnb.services;
+
+import com.b2la.hnb.models.facturation;
+
+import java.util.List;
+
+public class facturationService {
+    public void save(facturation facture){
+
+    }
+
+    public void update(facturation facture){
+
+    }
+
+    public facturation findById(Long id){
+
+    }
+
+    public List<facturation> findAll(){
+
+    }
+
+    public void delete (Long id){
+
+    }
+
+}
