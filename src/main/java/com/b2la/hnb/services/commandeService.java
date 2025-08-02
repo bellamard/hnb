06ce85cef4bdee6bfdbd2238052953cec9e0979f
B2LA <belamard@gmail.com>
2025-08-02 11:39:26 +0100

    ajout commande service

---
+++ src/main/java/com/b2la/hnb/services/commandeService.java
@@ -0,0 +1,28 @@
+package com.b2la.hnb.services;
+
+import com.b2la.hnb.models.commande;
+
+import java.util.List;
+
+public class commandeService {
+
+    public void save(commande article){
+
+    }
+
+    public void update(commande article){
+
+    }
+
+    public commande findById(Long ){
+
+    }
+
+    public List<commande> findAll(){
+
+    }
+
+    public void delete(Long id){
+
+    }
+}
