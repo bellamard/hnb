4803d09b1cfccbeabac1e996130761bfbdd1ec56
B2LA <belamard@gmail.com>
2025-08-02 11:09:28 +0100

    ajout fonction save sur service depense

--- src/main/java/com/b2la/hnb/services/depenseService.java
+++ src/main/java/com/b2la/hnb/services/depenseService.java
@@ -1,13 +1,28 @@
 package com.b2la.hnb.services;
 
 import com.b2la.hnb.models.depense;
+import com.b2la.hnb.util.JPAUtil;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityTransaction;
 
 import java.util.List;
 
 public class depenseService {
 
     public void save(depense depense){
-
+        EntityManager em= JPAUtil.getEntityManager();
+        EntityTransaction transaction=null;
+        try{
+            transaction = em.getTransaction();
+            transaction.begin();
+            em.persist(depense);
+            transaction.commit();
+        } catch (RuntimeException e) {
+            if(transaction!=null)transaction.rollback();
+            throw new RuntimeException(e);
+        }finally {
+            em.close();
+        }
     }
 
     public void update(depense depense){
