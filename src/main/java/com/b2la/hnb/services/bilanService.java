7fe9af987efba84f9189397b5b7b3f88e5188cf9
B2LA <belamard@gmail.com>
2025-08-02 12:04:13 +0100

    ajout service bilan

---
+++ src/main/java/com/b2la/hnb/services/bilanService.java
@@ -0,0 +1,27 @@
+package com.b2la.hnb.services;
+
+import com.b2la.hnb.models.bilan;
+
+import java.util.List;
+
+public class bilanService {
+
+    public void save(){
+
+    }
+
+    public void update(){
+
+    }
+    public bilan findById(Long id){
+
+    }
+
+    public List<bilan> findAll(){
+
+    }
+
+    public void delete(Long id){
+
+    }
+}
