6fe43ab3c472a55a6cc62316876d91a1c079b804
B2LA <belamard@gmail.com>
2025-08-02 11:42:09 +0100

    ajout fonction save sur commande service

--- src/main/java/com/b2la/hnb/services/commandeService.java
+++ src/main/java/com/b2la/hnb/services/commandeService.java
@@ -1,13 +1,28 @@
 package com.b2la.hnb.services;
 
 import com.b2la.hnb.models.commande;
+import com.b2la.hnb.util.JPAUtil;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityTransaction;
 
 import java.util.List;
 
 public class commandeService {
 
     public void save(commande article){
-
+        EntityManager em= JPAUtil.getEntityManager();
+        EntityTransaction transaction= null;
+        try {
+            transaction= em.getTransaction();
+            transaction.begin();
+            em.persist(article);
+            transaction.commit();
+        } catch (RuntimeException e) {
+            if(transaction!=null)transaction.rollback();
+            throw new RuntimeException(e);
+        }finally {
+            em.close();
+        }
     }
 
     public void update(commande article){
