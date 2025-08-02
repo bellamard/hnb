6105f63d56a88845576d37ed2dbdc0068a76df87
B2LA <belamard@gmail.com>
2025-08-02 11:05:27 +0100

    ajout sur service depense

---
+++ src/main/java/com/b2la/hnb/services/depenseService.java
@@ -0,0 +1,28 @@
+package com.b2la.hnb.services;
+
+import com.b2la.hnb.models.depense;
+
+import java.util.List;
+
+public class depenseService {
+
+    public void save(depense depense){
+
+    }
+
+    public void update(depense depense){
+
+    }
+
+    public depense findById(Long id){
+
+    }
+
+    public List<depense> findAll(){
+
+    }
+
+    public void delete(Long id){
+
+    }
+}
