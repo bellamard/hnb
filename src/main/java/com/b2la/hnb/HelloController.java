923a1a0434dbe4694f51a8cb6519290a64928c27
B2LA <belamard@gmail.com>
2025-08-03 15:08:45 +0100

    ajout fonction enter pour valider le formulaire

--- src/main/java/com/b2la/hnb/HelloController.java
+++ src/main/java/com/b2la/hnb/HelloController.java
@@ -4,6 +4,8 @@ import com.b2la.hnb.util.Stockage;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
 
 public class HelloController {
     @FXML
@@ -11,6 +13,16 @@ public class HelloController {
 
     @FXML
     private Button connexion, fermer;
+
+    @FXML
+    protected void handleKeyPressed(KeyEvent event){
+        if (event.getCode() == KeyCode.ENTER) {
+            onConnexion();
+        }
+    }
+
+    private void onConnexion() {
+    }
 
 
     @FXML
