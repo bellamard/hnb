8117356dfc456d4b1431dfff6c2348915d1631d5
B2LA <belamard@gmail.com>
2025-08-05 16:42:33 +0100

    ajout buttons dashboard

--- src/main/java/com/b2la/hnb/controllers/DashboardController.java
+++ src/main/java/com/b2la/hnb/controllers/DashboardController.java
@@ -5,6 +5,7 @@ import com.b2la.hnb.util.Stockage;
 import javafx.animation.KeyFrame;
 import javafx.animation.Timeline;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.util.Duration;
 
 import javafx.scene.control.Label;
@@ -14,11 +15,12 @@ import java.time.format.DateTimeFormatter;
 public class DashboardController {
     @FXML
     private Label username, fonction, dateHeure;
+    @FXML
+    private Button facturation, produit, cloture, depense, utilisateur;
 
     public void initialize() {
         recoveryUsername();
         viewDateTime();
-
     }
 
     public void recoveryUsername() {
