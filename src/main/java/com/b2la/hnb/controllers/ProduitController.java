c1308ca6582c4f20b090690650f60d2bdf68d6d7
bellamard kiala <belamard@outlook.fr>
2025-08-14 16:32:14 +0100

    ajoute fonction getAll  produitController

--- src/main/java/com/b2la/hnb/controllers/ProduitController.java
+++ src/main/java/com/b2la/hnb/controllers/ProduitController.java
@@ -1,11 +1,16 @@
 package com.b2la.hnb.controllers;
 
 import com.b2la.hnb.models.Produit;
+import com.b2la.hnb.services.produitService;
 import com.b2la.hnb.util.categoryType;
 import javafx.fxml.FXML;
 import javafx.scene.control.ComboBox;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 
 import java.awt.*;
+import java.util.List;
 
 public class ProduitController {
     @FXML
@@ -16,11 +21,24 @@ public class ProduitController {
     ComboBox<Integer> nbreArticle;
     @FXML
     ComboBox<categoryType> category;
+    @FXML
+    TableView<Produit> tableProduit;
+    TableColumn<Produit, String>Tnom, Tdescription;
+    TableColumn<Produit, Integer>Tquantite;
+    TableColumn<Produit, Double>Tprix;
+    TableColumn<Produit, categoryType>Ttype;
 
     @FXML
     Button btnAdd, btnAllDelete, btnValider;
 
+    produitService ps;
+
     public void getProduitAll(){
+        ps=new produitService();
+        List<Produit> produitList= ps.findAll();
+        Tnom.setCellValueFactory(new PropertyValueFactory<>("nom"));
+        Tdescription.setCellValueFactory(new PropertyValueFactory<>("description"));
+        Tprix.setCellValueFactory(new PropertyValueFactory<>("prix"));
 
     }
     public void getProduitSearch(String search){
@@ -30,6 +48,7 @@ public class ProduitController {
 
     }
     private void update(){
+
 
     }
     private void reset(){
