cff040dc46b5fd0bed6bc9ea6156d3dbb25eb3b0
bellamard kiala <belamard@outlook.fr>
2025-08-14 16:12:54 +0100

    ajoute fonction reset  produitController

--- src/main/java/com/b2la/hnb/controllers/ProduitController.java
+++ src/main/java/com/b2la/hnb/controllers/ProduitController.java
@@ -1,18 +1,42 @@
 package com.b2la.hnb.controllers;
 
 import com.b2la.hnb.models.Produit;
+import com.b2la.hnb.util.categoryType;
+import javafx.fxml.FXML;
+import javafx.scene.control.ComboBox;
+
+import java.awt.*;
 
 public class ProduitController {
-    private void getProduitAll(){
+    @FXML
+    TextField research, articleField, prixField;
+    @FXML
+    TextArea description;
+    @FXML
+    ComboBox<Integer> nbreArticle;
+    @FXML
+    ComboBox<categoryType> category;
+
+    @FXML
+    Button btnAdd, btnAllDelete, btnValider;
+
+    public void getProduitAll(){
 
     }
-    private void getProduitSearch(String search){
+    public void getProduitSearch(String search){
 
     }
-    private void deleteProduit(Long id){
+    public void deleteProduit(Long id){
 
     }
     private void update(){
 
+    }
+    private void reset(){
+        nbreArticle.setValue(0);
+        category.setValue(categoryType.plat);
+        articleField.setText("");
+        prixField.setText("0");
+        description.setText("");
     }
 }
