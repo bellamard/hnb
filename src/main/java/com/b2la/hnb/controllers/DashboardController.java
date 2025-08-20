7e8dd9867c2aab9b420a3943281a5a3df9e40ea0
bellamard kiala <belamard@outlook.fr>
2025-08-20 10:27:02 +0100

    ajout fonction selection article sur la facturation

--- src/main/java/com/b2la/hnb/controllers/DashboardController.java
+++ src/main/java/com/b2la/hnb/controllers/DashboardController.java
@@ -29,31 +29,34 @@ import static javafx.application.Platform.runLater;
 
 public class DashboardController {
     @FXML
-    private Label username, fonction, dateHeure, PanneauDashboardProduit, PanneauDashboardFacture;
+    private Label username, fonction, dateHeure, PanneauDashboardProduit, PanneauDashboardFacture, labelDescription;
     @FXML
-    private Button home,facturation, produit, cloture, depense, utilisateur, btnProduitMod, btnProduitAdd;
+    private Button home, facturation, produit, cloture, depense, utilisateur, btnProduitMod, btnProduitAdd;
     @FXML
-    private VBox homeLayout,facturationLayout, produitLayout, depenseLayout, clotureLayout, parametreLayout, loadingLayout;
+    private VBox homeLayout, facturationLayout, produitLayout, depenseLayout, clotureLayout, parametreLayout, loadingLayout;
 
     @FXML
-    TextField research, articleField, prixField;
+    TextField research, articleField, prixField, researchFacture;
     @FXML
     TextArea description;
     @FXML
     ComboBox<Integer> nbreArticle;
+
+    @FXML
+    ComboBox<Integer> produitComboBox;
     @FXML
     ComboBox<categoryType> category;
     @FXML
     TableView<Produit> tableProduit, articlesTable;
 
     @FXML
-    TableColumn<Produit, String>Tarticle, Tdescription, Taction, produitArticle;
+    TableColumn<Produit, String> Tarticle, Tdescription, Taction, produitArticle;
     @FXML
-    TableColumn<Produit, Integer>Tquantite, quantiteArticle;
+    TableColumn<Produit, Integer> Tquantite, quantiteArticle;
     @FXML
-    TableColumn<Produit, Double>Tprix, prixUnitaireArticle;
+    TableColumn<Produit, Double> Tprix, prixUnitaireArticle;
     @FXML
-    TableColumn<Produit, categoryType>Ttype;
+    TableColumn<Produit, categoryType> Ttype;
 
     @FXML
     Button btnAdd, btnAllDelete, btnValider;
@@ -63,7 +66,7 @@ public class DashboardController {
     produitService ps;
     facturationService fs;
     Long idProduit;
-    Double dashBoardFacture=0.0, dashBoardProduit=0.0;
+    Double dashBoardFacture = 0.0, dashBoardProduit = 0.0;
 
     String commande;
     Double prixUnitcomm;
@@ -78,16 +81,16 @@ public class DashboardController {
             nbreArticle.getItems().add(i);
         }
         category.getItems().addAll(categoryType.values());
-        ps=new produitService();
-        fs= new facturationService();
-        dashBoardProduit= (double) ps.numberProduit();
-        dashBoardFacture=fs.sommeFacture();
+        ps = new produitService();
+        fs = new facturationService();
+        dashBoardProduit = (double) ps.numberProduit();
+        dashBoardFacture = fs.sommeFacture();
         dashBoardNumber();
 
     }
 
-    private void dashBoardNumber(){
-        runLater(()->{
+    private void dashBoardNumber() {
+        runLater(() -> {
             PanneauDashboardProduit.setText(String.valueOf(dashBoardProduit));
             PanneauDashboardFacture.setText(String.valueOf(dashBoardFacture));
         });
@@ -113,8 +116,8 @@ public class DashboardController {
     }
 
 
-    private void cardLayout(String layout){
-        switch (layout){
+    private void cardLayout(String layout) {
+        switch (layout) {
             case "home":
                 homeLayout.setVisible(true);
                 produitLayout.setVisible(false);
@@ -175,105 +178,125 @@ public class DashboardController {
 
         }
     }
+
     @FXML
-    private void homeView(){
-        String layout="home";
+    private void homeView() {
+        String layout = "home";
         cardLayout(layout);
     }
 
     @FXML
-    private void factureView(){
-        String layout="facture";
+    private void factureView() {
+        String layout = "facture";
         cardLayout(layout);
+        getAllProduit();
     }
 
     @FXML
-    private void produitView(){
-        String layout="produit";
+    private void produitView() {
+        String layout = "produit";
         getProduitAll();
         cardLayout(layout);
     }
 
     @FXML
-    private void depenseView(){
-        String layout="depense";
+    private void depenseView() {
+        String layout = "depense";
         cardLayout(layout);
     }
 
     @FXML
-    private void clotureView(){
-        String layout="cloture";
+    private void clotureView() {
+        String layout = "cloture";
         cardLayout(layout);
     }
 
     @FXML
-    private void parametreView(){
-        String layout="parametre";
+    private void parametreView() {
+        String layout = "parametre";
         cardLayout(layout);
     }
-    @FXML
-    private void annulerFacture(){
 
-    }
     @FXML
-    private void genererFacture(){
-
-    }
-    @FXML
-    private void ajouterArticle(){
+    private void annulerFacture() {
 
     }
 
-    private void getAllProduit(){
-        ps=new produitService();
-        List<Produit> prodList= ps.findAll();
+    @FXML
+    private void genererFacture() {
+
+    }
+
+    @FXML
+    private void ajouterArticle() {
+
+    }
+
+    private void getAllProduit() {
+        ps = new produitService();
+        List<Produit> prodList = ps.findAll();
         produitArticle.setCellValueFactory(new PropertyValueFactory<>("nom"));
         prixUnitaireArticle.setCellValueFactory(new PropertyValueFactory<>("prixUnitaire"));
         quantiteArticle.setCellValueFactory(new PropertyValueFactory<>("quantiteStock"));
-        Task<ObservableList<Produit>> task= new Task<>(){
+
+        articlesTable.getSelectionModel().selectedItemProperty().addListener((obs, oldSelection, newSelection) -> {
+            if (newSelection != null) {
+                System.out.println("Ligne sélectionnée : " + newSelection.getNom() + ", Prix : " + newSelection.getPrixUnitaire());
+                idComm = newSelection.getId();
+                commande = newSelection.getNom();
+                prixUnitcomm = newSelection.getPrixUnitaire();
+                researchFacture.setText(newSelection.getNom());
+                descriptionFacturation(commande, prixUnitcomm,0);
+                produitComboBox.getItems().removeAll();
+                for (int i = 0; i <=newSelection.getQuantiteStock() ; i++) {
+                    produitComboBox.getItems().add(i);
+                }
+                produitComboBox.setOnAction(
+                        actionEvent -> {
+                            descriptionFacturation(commande, prixUnitcomm, produitComboBox.getValue());
+                        }
+                );
+
+            }
+        });
+        Task<ObservableList<Produit>> task = new Task<>() {
 
             @Override
             protected ObservableList<Produit> call() throws Exception {
                 return FXCollections.observableArrayList(prodList);
             }
         };
-        articlesTable.getSelectionModel().selectedItemProperty().addListener((obs, oldSelection, newSelection) -> {
-            if (newSelection != null) {
-                System.out.println("Ligne sélectionnée : " + newSelection.getName() + ", Âge : " + newSelection.getAge());
-                idComm= newSelection.getId();
-                commande= newSelection.getNom();
-                prixUnitcomm= newSelection.getPrixUnitaire();
-            }
-        });
-        task.setOnSucceeded(e->articlesTable.setItems(task.getValue()));
+        task.setOnSucceeded(e -> articlesTable.setItems(task.getValue()));
         new Thread(task).start();
     }
+
     @FXML
-    public void getProduitAll(){
-        ps=new produitService();
-        List<Produit> produitList= ps.findAll();
-        dashBoardProduit= (double) ps.numberProduit();
+    public void getProduitAll() {
+        ps = new produitService();
+        List<Produit> produitList = ps.findAll();
+        dashBoardProduit = (double) ps.numberProduit();
         dashBoardNumber();
         Tarticle.setCellValueFactory(new PropertyValueFactory<>("nom"));
         Tdescription.setCellValueFactory(new PropertyValueFactory<>("description"));
         Tprix.setCellValueFactory(new PropertyValueFactory<>("prixUnitaire"));
         Tquantite.setCellValueFactory(new PropertyValueFactory<>("quantiteStock"));
         Ttype.setCellValueFactory(new PropertyValueFactory<>("type"));
-        Taction.setCellFactory(col->new TableCell<>(){
-            Button btnMod= new Button("Mod");
-            Button btnSup= new Button("Sup");
+        Taction.setCellFactory(col -> new TableCell<>() {
+            Button btnMod = new Button("Mod");
+            Button btnSup = new Button("Sup");
+
             {
 
-                btnMod.setOnAction(e->{
-                    Produit produitItem= getTableView().getItems().get(getIndex());
+                btnMod.setOnAction(e -> {
+                    Produit produitItem = getTableView().getItems().get(getIndex());
                     System.out.println(produitItem.getNom());
                     getProduit(produitItem);
                     btnProduitMod.setVisible(true);
                     btnProduitAdd.setVisible(false);
                 });
 
-                btnSup.setOnAction(e->{
-                    Produit produitItem= getTableView().getItems().get(getIndex());
+                btnSup.setOnAction(e -> {
+                    Produit produitItem = getTableView().getItems().get(getIndex());
                     System.out.println(produitItem.getNom());
                     askSupression(produitItem.getId());
                 });
@@ -282,30 +305,30 @@ public class DashboardController {
             @Override
             protected void updateItem(String item, boolean empty) {
                 super.updateItem(item, empty);
-                if(empty)setGraphic(null);
-                else{
-                    HBox boxBtn=new HBox(2);
-                    boxBtn.getChildren().addAll(btnSup,btnMod);
+                if (empty) setGraphic(null);
+                else {
+                    HBox boxBtn = new HBox(2);
+                    boxBtn.getChildren().addAll(btnSup, btnMod);
                     setGraphic(boxBtn);
 
                 }
             }
         });
 
-        Task<ObservableList<Produit>> task= new Task<>(){
+        Task<ObservableList<Produit>> task = new Task<>() {
 
             @Override
             protected ObservableList<Produit> call() throws Exception {
                 return FXCollections.observableArrayList(produitList);
             }
         };
-        task.setOnSucceeded(e->tableProduit.setItems(task.getValue()));
+        task.setOnSucceeded(e -> tableProduit.setItems(task.getValue()));
         new Thread(task).start();
         resetProduit();
     }
 
-    private void getProduit(Produit produit){
-        idProduit=produit.getId();
+    private void getProduit(Produit produit) {
+        idProduit = produit.getId();
         articleField.setText(produit.getNom());
         description.setText(produit.getDescription());
         prixField.setText(String.valueOf(produit.getPrixUnitaire()));
@@ -313,7 +336,7 @@ public class DashboardController {
         category.setValue(produit.getType());
     }
 
-    private void resetProduit(){
+    private void resetProduit() {
         articleField.setText("");
         prixField.setText("");
         description.setText("");
@@ -324,36 +347,36 @@ public class DashboardController {
     }
 
     @FXML
-    public void getProduitSearch(){
-        String search=research.getText();
-        List<Produit> produitList=ps.findAll().stream().filter(produit ->
+    public void getProduitSearch() {
+        String search = research.getText();
+        List<Produit> produitList = ps.findAll().stream().filter(produit ->
                 produit.getNom().toLowerCase().contains(search.toLowerCase()) ||
-                        produit.getDescription().toLowerCase().contains(search.toLowerCase())||
-                        String.valueOf(produit.getPrixUnitaire()).toLowerCase().contains(search.toLowerCase())||
+                        produit.getDescription().toLowerCase().contains(search.toLowerCase()) ||
+                        String.valueOf(produit.getPrixUnitaire()).toLowerCase().contains(search.toLowerCase()) ||
                         String.valueOf(produit.getQuantiteStock()).toLowerCase().contains(search.toLowerCase())
         ).collect(Collectors.toList());
 
-        Task<ObservableList<Produit>> task= new Task<ObservableList<Produit>>() {
+        Task<ObservableList<Produit>> task = new Task<ObservableList<Produit>>() {
             @Override
             protected ObservableList<Produit> call() throws Exception {
                 return FXCollections.observableArrayList(produitList);
             }
         };
-        task.setOnSucceeded(e->tableProduit.setItems(task.getValue()));
+        task.setOnSucceeded(e -> tableProduit.setItems(task.getValue()));
         new Thread(task).start();
         resetProduit();
     }
 
     @FXML
-    public void deleteAllProduit(){
+    public void deleteAllProduit() {
 
         Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
         alert.setTitle("Confirmation");
         alert.setHeaderText("Voulez-vous Supprimer tout les produits?");
         alert.setContentText("Cliquez sur Ok pour confirmer la suppression.");
-        Optional<ButtonType> result= alert.showAndWait();
-        if(result.isPresent()&& result.get()== ButtonType.OK){
-            List<Produit> produitList=ps.findAll();
+        Optional<ButtonType> result = alert.showAndWait();
+        if (result.isPresent() && result.get() == ButtonType.OK) {
+            List<Produit> produitList = ps.findAll();
             for (Produit produit : produitList) {
                 ps.delete(produit.getId());
             }
@@ -363,74 +386,82 @@ public class DashboardController {
     }
 
     @FXML
-    private void updateProduit(){
-        if(articleField.getText().isEmpty())messageErreur("Votre champs nom est vide!!!");
+    private void updateProduit() {
+        if (articleField.getText().isEmpty()) messageErreur("Votre champs nom est vide!!!");
 //        if(description.getText().isEmpty())messageErreur("Votre champs description est vide!!!");
-        if(prixField.getText().isEmpty())messageErreur("votre champs prix est vide!!!");
-        if(category.getValue().toString().isEmpty())messageErreur("Votre categorie est vide!!!");
-        if(nbreArticle.getValue().toString().isEmpty())messageErreur("Votre nombre d'article est vide!!!");
-        Produit produit= new Produit();
+        if (prixField.getText().isEmpty()) messageErreur("votre champs prix est vide!!!");
+        if (category.getValue().toString().isEmpty()) messageErreur("Votre categorie est vide!!!");
+        if (nbreArticle.getValue().toString().isEmpty()) messageErreur("Votre nombre d'article est vide!!!");
+        Produit produit = new Produit();
         produit.setId(idProduit);
         produit.setNom(articleField.getText());
         produit.setDescription(description.getText());
         produit.setType(category.getValue());
         produit.setQuantiteStock(nbreArticle.getValue());
         produit.setPrixUnitaire(Double.parseDouble(prixField.getText()));
-        ps= new produitService();
+        ps = new produitService();
         ps.update(produit);
         getProduitAll();
 
     }
 
     @FXML
-    private void addProduit(){
-        if(articleField.getText().isEmpty())messageErreur("Votre champs nom est vide!!!");
+    private void addProduit() {
+        if (articleField.getText().isEmpty()) messageErreur("Votre champs nom est vide!!!");
 //        if(description.getText().isEmpty())messageErreur("Votre champs description est vide!!!");
-        if(prixField.getText().isEmpty())messageErreur("votre champs prix est vide!!!");
-        if(category.getValue().toString().isEmpty())messageErreur("Votre categorie est vide!!!");
-        if(nbreArticle.getValue().toString().isEmpty())messageErreur("Votre nombre d'article est vide!!!");
-        Produit produit= new Produit();
+        if (prixField.getText().isEmpty()) messageErreur("votre champs prix est vide!!!");
+        if (category.getValue().toString().isEmpty()) messageErreur("Votre categorie est vide!!!");
+        if (nbreArticle.getValue().toString().isEmpty()) messageErreur("Votre nombre d'article est vide!!!");
+        Produit produit = new Produit();
         produit.setNom(articleField.getText());
         produit.setDescription(description.getText());
         produit.setType(category.getValue());
         produit.setQuantiteStock(nbreArticle.getValue());
         produit.setPrixUnitaire(Double.parseDouble(prixField.getText()));
-        ps= new produitService();
+        ps = new produitService();
         ps.save(produit);
-        boiteAlert("felicitation vous avez enregistre le produit "+produit.getNom());
+        boiteAlert("felicitation vous avez enregistre le produit " + produit.getNom());
         getProduitAll();
 
     }
 
-    private void boiteAlert(String message){
+    private void boiteAlert(String message) {
         Alert alert = new Alert(Alert.AlertType.INFORMATION);
         alert.setTitle("Information");
-        alert.setHeaderText(message.toUpperCase().substring(0,10));
+        alert.setHeaderText(message.toUpperCase().substring(0, 10));
         alert.setContentText(message);
         alert.showAndWait();
     }
 
-    private void messageErreur(String message){
+    private void messageErreur(String message) {
 
         runLater(() -> {
             msgAlert.setText(message);
-        } );
+        });
         throw new RuntimeException(message);
     }
 
-    private void askSupression(Long produitId){
+    private void askSupression(Long produitId) {
         Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
         alert.setTitle("Confirmation");
         alert.setHeaderText("Voulez-vous Supprimer ?");
         alert.setContentText("Cliquez sur Ok pour confirmer la suppression.");
-        Optional<ButtonType> result= alert.showAndWait();
-        if(result.isPresent()&& result.get()== ButtonType.OK){
+        Optional<ButtonType> result = alert.showAndWait();
+        if (result.isPresent() && result.get() == ButtonType.OK) {
             ps.delete(produitId);
             System.out.println("suppression valider");
             getProduitAll();
         }
     }
 
+    private void descriptionFacturation(String nomProduit, double prixUnitaire, int quantite) {
+        double total = prixUnitaire * quantite;
+        labelDescription.setText(String.format(
+                "Produit : %s\nPrix    : %.2f €\nQuantité: %d pièces\nTotal   : %.2f €",
+                        nomProduit, prixUnitaire, quantite, total
+                )
+        );
+    }
 
 
 }
