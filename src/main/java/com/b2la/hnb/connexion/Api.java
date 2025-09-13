59d04675c80b56a507f279f5ce9716dd39e81e8f
bellamard kiala <belamard@outlook.fr>
2025-09-13 13:33:04 +0100

    configurer en api

---
+++ src/main/java/com/b2la/hnb/connexion/Api.java
@@ -0,0 +1,43 @@
+package com.b2la.hnb.connexion;
+
+import com.b2la.hnb.models.Bilan;
+import com.b2la.hnb.models.Depense;
+import com.b2la.hnb.models.Facturation;
+import com.b2la.hnb.models.Produit;
+
+import java.util.List;
+
+public class Api {
+    public static List<Produit> listProduit(){
+        return ;
+    }
+
+    public static List<Produit> addList(List<Produit> produit){
+        return ;
+    }
+
+    public static List<Facturation> listFacture(){
+        return ;
+    }
+
+    public static List<Facturation> addListFacture(List<Facturation> facturations){
+        return ;
+    }
+
+    public static List<Depense> listDepense(){
+        return ;
+    }
+
+    public static List<Depense> addListDepense(List<Depense> depenses){
+        return ;
+    }
+
+    public static List<Bilan> listBilan(){
+        return ;
+    }
+
+    public static List<Bilan> addListBilan(List<Bilan> bilan){
+        return ;
+    }
+
+}
